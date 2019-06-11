3fb7e353901b1dce41499b1f43f32963a8d7041e
Brian <wl00387719@gmail.com>
2019-06-11 19:31:22 +0800

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const keys = require('./config/keys')
+// define user model in mongoDB
+require('./models/User')
+// pull out user model
+require('./sevices/passport')
+
+console.log('conneting MongoDB')
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+  .then(res => console.log('DB connected'))
+  .catch(err => console.log('DB connect fail: ', err))
+
 const app = express()
 
-app.get('/', function (req, res, next) {
-  res.send({ hi: 'there'})
-})
+app.use(
+  cookieSession({
+    // we want this cookie to last 30 days
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
